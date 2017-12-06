ff19f3f738231d415063a7525250c46f93e71d01
Michael Chen <cyjdyj@gmail.com>
2017-12-06 11:19:36 -0800

    server setup

--- app.js
+++ app.js
@@ -1,6 +1,58 @@
 const express = require('express')
 const app = express()
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const http = require('http');
+const mongoose = require('mongoose');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//middleware
+app.use(logger('dev')); // log requests in server console
+app.use(bodyParser.json()); // parse client request data to json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// connect mongoDB
+mongoose.connect('mongodb://localhost:27017/webdxd', { useMongoClient:true });
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err =  new Error('Page Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next)=> {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: err
+  });
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, ()=>{
+    console.log(`Server running on http://localhost:${port}`)
+});
