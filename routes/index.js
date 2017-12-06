f0b0ea94fee9f5075c8692e4c77ec0d86f6d4b42
Michael Chen <cyjdyj@gmail.com>
2017-12-06 11:49:37 -0800

    add students model

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const Students = require('../models/students');
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Welcome to Full-stack !' })
-})
+  Students.find({}, (err, students) => {
+    res.render('index', { title: 'Welcome to Full-stack !', name: 'Michael', students })
+  });
+});
 
 module.exports = router;
