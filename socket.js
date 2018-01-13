26353933623a55da45d674355204118c1149faab
Michael Chen <cyjdyj@gmail.com>
2018-01-12 17:42:34 -0800

    display online users

--- socket.js
+++ socket.js
@@ -1,9 +1,10 @@
 const Chats = require('./models/chats');
 
 module.exports = (io) => {
+  const onlineUsers = [];
   io.on('connection', (socket) => {
     console.log('connected');
-    socket.on('chat message', obj => {
+    socket.on('chat message', (obj) => {
       const chat = new Chats(obj);
       chat.save((err, doc) => {
         if(err){
@@ -11,6 +12,18 @@ module.exports = (io) => {
         } else {
           io.emit('chat broadcast', obj)
         }
+      })
+    })
+
+    socket.on('send id', (user) => {
+      onlineUsers.push(user);
+
+      io.emit('send users', onlineUsers)
+
+      socket.on('disconnect', () => {
+        const userIndex = onlineUsers.indexOf(user);
+        onlineUsers.splice(userIndex, 1);
+        io.emit('send users', onlineUsers)
       })
     })
   });
